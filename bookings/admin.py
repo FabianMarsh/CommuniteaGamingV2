e57e082c0e6971eab6d5edbc21229fba4b2aa3c3
FabianMarsh <fabian.marsh00@gmail.com>
2025-05-02 16:32:32 +0000

    add bookings views and templates

--- bookings/admin.py
+++ bookings/admin.py
@@ -4,4 +4,9 @@ from .models import Table, TimeSlot, Booking
 # Register your models here.
 admin.site.register(Table)
 admin.site.register(TimeSlot)
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_display = ('time',) # Show time field in the admin list
+
+    ordering = ['time'] # Ensure sorting in the admin panel
+
 admin.site.register(Booking)
--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Table, TimeSlot
 
 # Create your views here.
+
+def bookings(request):
+    """ A view to return the bookings page """
+
+    tables = Table.objects.all()
+    times = TimeSlot.objects.all()
+
+    context = {
+        'tables': tables,
+        'times': times,
+    }
+
+    return render(request, 'bookings/bookings.html', context)
