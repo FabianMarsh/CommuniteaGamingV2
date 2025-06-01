8b8a6101a7923e0369d7aa2090f6b3c3a1abb30c
FabianMarsh <fabian.marsh00@gmail.com>
2025-06-01 13:49:11 +0000

    updated allowed hosts and CSRF origins to correct format

--- communitea_gaming/settings.py
+++ communitea_gaming/settings.py
@@ -29,13 +29,13 @@ SECRET_KEY = 'django-insecure-z_sf1r_t6xkt@_w%34sadt)6%do)r)sf=4g2fof_h&-!$)%4@(
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-fabianmarsh-communiteag-ukq996bcmrb.ws-eu120.gitpod.io',
-    'communiteagaming-c8bf0b2e702e.herokuapp.com'
+    'https://8000-fabianmarsh-communiteag-ukq996bcmrb.ws-eu120.gitpod.io',
+    'https://communiteagaming-c8bf0b2e702e.herokuapp.com'
     ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://8000-fabianmarsh-communiteag-ukq996bcmrb.ws-eu120.gitpod.io",
-    'communiteagaming-c8bf0b2e702e.herokuapp.com'
+    'https://communiteagaming-c8bf0b2e702e.herokuapp.com'
     ]
 
 
